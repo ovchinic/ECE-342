625730910747ca434b9a0dd07df8ba2087f6f39e
Christian Ovchinikov <101433243+ovchinic@users.noreply.github.com>
2022-03-11 15:34:17 -0800

    Update Microcontroller_SourceCode.ino

--- Microcontroller_SourceCode.ino
+++ Microcontroller_SourceCode.ino
@@ -2,18 +2,22 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_MLX90614.h>
+
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C // Screen Address in memory
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); // Initializes the OLED screen
 Adafruit_MLX90614 mlx = Adafruit_MLX90614(); // Initializes the ir sensor
+
 int incomingByte; // Reads incoming serial data
 const int buzzer = 2; // Constant value of 2 indicating D2 output on Arduino Nanoe
 float finalTemp; // Stores final temp
 char finalUnit; // Stores unit used
 double tempRead;
 double tempAdj;
+
 void setup() {
   Serial.begin(9600); // Starts serial comm with baud rate of 9600
   mlx.begin(); // Starts the IR temperature reading
@@ -24,6 +28,7 @@ void setup() {
 void loop() {
   int counter = 200; // Counter used to record temperatures for around 6.5 seconds
   display.clearDisplay();
+  
   // Text at the top of the screen
   display.setTextSize(2);
   display.setTextColor(WHITE);
@@ -35,20 +40,24 @@ void loop() {
   if (Serial.available() > 0) {
     // read the oldest byte in the serial buffer:
     incomingByte = Serial.read();
+    
     // if it's a capital C (ASCII 67), print to display in Celsius:
     if (incomingByte == 'C') {
       // Initial beep to let the user know that their temperature is being recorded
       digitalWrite(buzzer, HIGH); // Turn on Buzzer
       delay(100); // Delay for 1/10 sec
       digitalWrite(buzzer, LOW); // Turn on Buzzer
+      
       // Loops until counter = 0, which is approximately 5 seconds
       while (counter) {
         display.clearDisplay();
+        
         // Text at the top of the screen
         display.setTextSize(1);
         display.setTextColor(WHITE);
         display.setCursor(0, 0);
         display.println("Temperature:");
+        
         // Text in the middle of the temperature reading
         display.setTextSize(3);
         display.setCursor(10, 16);
@@ -57,11 +66,13 @@ void loop() {
         display.println(tempAdj, 1);
         display.setCursor(90, 16);
         display.println("C");
+        
         // Wait text at the bottom of the screen
         display.setTextSize(1);
         display.setTextColor(WHITE);
         display.setCursor(0, 45);
         display.println("Wait for the tone");
+        
         finalTemp = tempAdj;
         counter = counter - 1;
         if (counter % 2 == 0)
@@ -71,34 +82,39 @@ void loop() {
       Serial.print(finalTemp, 1);
       FinalPrint();
     }
+    
     // if it's an F (ASCII 70), print to display in Fahrenheit:
     if (incomingByte == 'F') {
       // Initial beep to let the user know that their temperature is being recorded
       digitalWrite(buzzer, HIGH); // Turn on Buzzer
       delay(100); // Delay for 1/10 sec
       digitalWrite(buzzer, LOW); // Turn on Buzzer
+      
       // Loops until counter = 0, which is approximately 5 seconds
       while (counter) {
         display.clearDisplay();
+        
         // Text at the top of the screen
         display.setTextSize(1);
         display.setTextColor(WHITE);
         display.setCursor(0, 0);
         display.println("Temperature:");
+        
         // Text in the middle of the temperature reading
         display.setTextSize(3);
         display.setCursor(10, 16);
         tempRead = mlx.readObjectTempF();
-        13
         tempAdj = 4.53 + tempRead;
         display.println(tempAdj, 1);
         display.setCursor(90, 16);
         display.println("F");
+        
         // Wait text at the bottom of the screen
         display.setTextSize(1);
         display.setTextColor(WHITE);
         display.setCursor(0, 45);
         display.println("Wait for the tone");
+        
         finalTemp = tempAdj;
         counter = counter - 1;
         if (counter % 2 == 0)
@@ -115,13 +131,16 @@ void FinalPrint() {
   // Final print screen with the buzzer in the background to tell the user they are completed
   display.clearDisplay();
   display.display();
+  
   // Prints final temperature of user
   display.setTextSize(3);
   display.setCursor(10, 16);
   display.println(finalTemp, 1);
   display.setCursor(90, 16);
+  
   if (finalUnit == 'C') {
     display.println("C");
+    
     // Checks and prints if the user has a fever of over 38 C
     if (finalTemp > 33.7) {
       display.setTextSize(2);
@@ -129,6 +148,7 @@ void FinalPrint() {
       display.setCursor(0, 45);
       display.println(" FEVER");
       display.display();
+      
       // If fever beep faster
       digitalWrite(buzzer, HIGH); // Turn on Buzzer
       delay(250); // Delay for 1/4 sec
@@ -149,11 +169,9 @@ void FinalPrint() {
       digitalWrite(buzzer, HIGH);
       delay(250);
       digitalWrite(buzzer, LOW);
-      delay(2500); // Total delay of 5 seconds for user to see their final
-      temperature
+      delay(2500); // Total delay of 5 seconds for user to see their final temperature
     }
     else {
-      14
       display.display();
       digitalWrite(buzzer, HIGH); // Turn on Buzzer
       delay(500); // Delay for 1/2 sec
@@ -166,12 +184,12 @@ void FinalPrint() {
       digitalWrite(buzzer, HIGH);
       delay(500);
       digitalWrite(buzzer, LOW);
-      delay(2500); // Total delay of 5 seconds for user to see their final
-      temperature
+      delay(2500); // Total delay of 5 seconds for user to see their final temperature
     }
   }
   else if (finalUnit == 'F') {
     display.println("F");
+    
     // Checks and prints if the user has a fever of over 100.4 F
     if (finalTemp > 92.7) {
       display.setTextSize(2);
@@ -179,6 +197,7 @@ void FinalPrint() {
       display.setCursor(0, 45);
       display.println(" FEVER");
       display.display();
+      
       // If fever beep faster
       digitalWrite(buzzer, HIGH); // Turn on Buzzer
       delay(250); // Delay for 1/4 sec
